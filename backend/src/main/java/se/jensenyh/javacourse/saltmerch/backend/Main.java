cb32a4d66a80234f93a180c83ace40b205a83f77
samyuktha215 <samyuktha.basam@gmail.com>
2023-01-25 16:59:26 +0100

    initial commit

--- backend/src/main/java/se/jensenyh/javacourse/saltmerch/backend/Main.java
+++ backend/src/main/java/se/jensenyh/javacourse/saltmerch/backend/Main.java
@@ -1,10 +1,11 @@
-package src.main.java.se.jensenyh.javacourse.saltmerch.backend;
+package se.jensenyh.javacourse.saltmerch.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Main
+public class Main extends SpringBootServletInitializer
 {
     public static void main(String[] args)
     {
